6af4b573dff83b13ba17677e64ad77e22f873a01
PabloOscarSuarez <pablooscarsuarez@gmail.com>
2019-05-05 14:25:02 -0300

    ejercicio de selectores terminados

--- src/selector.js
+++ src/selector.js
@@ -1,25 +1,42 @@
 var traverseDomAndCollectElements = function(matchFunc, startEl) {
   var resultSet = [];
 
-  if (typeof startEl === "undefined") {
-    startEl = document.body;
-  }
-
+      if (typeof startEl === "undefined") {
+        startEl = document.body;
+      }
+  
+      if (matchFunc(startEl)){
+        resultSet.push(startEl);
+      }
+      if (startEl.children){
+        for (var i=0 ; i<startEl.children.length; i++){
+          resultSet = resultSet.concat(traverseDomAndCollectElements(matchFunc, startEl.children[i]));
+        };
+      };
+  
+  
+    
+  
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-
+  console.log(resultSet);
+  
   return resultSet;
-};
 
+};
 // Detecta y devuelve el tipo de selector
 // devuelve uno de estos tipos: id, class, tag.class, tag
 
 
 var selectorTypeMatcher = function(selector) {
-  // tu código aquí
-};
+  var copia = selector.slice();
+  if(selector[0]=="#")return "id";
+  else if(selector[0]==".")return "class";
+  else if(copia.split(".").length==2)return "tag.class";
+  else if (copia.split(".").length==1)return "tag";
+  }
 
 // NOTA SOBRE LA FUNCIÓN MATCH
 // recuerda, la función matchFunction devuelta toma un elemento como un
@@ -30,16 +47,44 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") {
+    matchFunction = function(elemento){
+      var arrSelector = selector.split("#");
+      return elemento.id && elemento.id==arrSelector[1];
+    };
     // define matchFunction para id
 
   } else if (selectorType === "class") {
-    // define matchFunction para class
+      matchFunction = function(elemento){
+        var coin= false
+        var arrSelector = selector.split(".")
+        for (let index = 0; index < elemento.classList.length && coin===false; index++) {
+          if (elemento.classList[index]==arrSelector[1]) {
+            coin =true
+            return coin
+          }
+          
+        }
+        return coin 
+
+        };
+    //     selector
+    //     var arrSelector = selector.split(".");
+    //     return elemento.className && elemento.className==arrSelector[1];
+    // };
 
   } else if (selectorType === "tag.class") {
-    // define matchFunction para tag.class
+    matchFunction = function(){};
+    var arrTagClass= selector.split(".")
+    var newClass = "."+arrTagClass[1];
+    console.log(arrTagClass[0] + " " + newClass)
+    return matchFunctionMaker(arrTagClass[0]) && matchFunctionMaker(newClass)
 
+    // define matchFunction para tag.class
   } else if (selectorType === "tag") {
-    // define matchFunction para tag
+    matchFunction = function(elemento){ 
+      return elemento.tagName && elemento.tagName.toLowerCase()==selector.toLowerCase();
+    };
+
 
   }
   return matchFunction;
